26f38f464778a163202c0437e8558f40f5832a7a
shivam <shivam.kumar>
2017-07-08 15:42:45 +0530

    Changed message in GetMethod.js file

--- GetMethod.js
+++ GetMethod.js
@@ -2,7 +2,7 @@
  * author: Shivam kumar singh
  */
 
- //A simple program to explain get method in javascript
+ //A simple program to explain get method in javascript/Node
  let http = new XMLHttpRequest();
  http.onreadystatechange = () => {
     if (http.readyState === 4 && http.status === 200) {
